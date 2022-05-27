59c5f4d5133e6a1be794ec806a99b78e215c67e3
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-27 15:38:10 -0500

    adds server functionality

--- app/src/main/java/com/ikarabulut/EchoServer.java
+++ app/src/main/java/com/ikarabulut/EchoServer.java
@@ -1,2 +1,49 @@
-package com.ikarabulut;public class EchoServer {
+package com.ikarabulut;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    private Socket clientSocket;
+    private ServerSocket serverSocket;
+    private PrintWriter echoWriter;
+    private BufferedReader echoReader;
+
+    public EchoServer(ServerSocket connectedServerSocket) {
+        serverSocket = connectedServerSocket;
+    }
+
+    public void createServerEndPoint(ServerIO io) throws IOException {
+        clientSocket = createClientSocket(serverSocket);
+        echoWriter = io.generateClientSocketWriter(clientSocket);
+        echoReader = io.generateClientSocketReader(clientSocket);
+    }
+
+    public void beginEcho() throws IOException {
+        String inputLine;
+        while ((inputLine = echoReader.readLine()) != null) {
+            if (".".equals(inputLine)) {
+                System.out.println("Thank you for trying my Echo Server");
+                break;
+            }
+            echoWriter.println(inputLine);
+        }
+    }
+
+    public Socket createClientSocket(ServerSocket serverSocket) {
+        try {
+            return serverSocket.accept();
+        } catch (IOException ex) {
+            System.err.print(ex.getMessage());
+        }
+        return null;
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
 }
--- app/src/test/java/com/ikarabulut/EchoServerTest.java
+++ app/src/test/java/com/ikarabulut/EchoServerTest.java
@@ -1,4 +1,38 @@
+package com.ikarabulut;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class EchoServerTest {
-  
+    @Test
+    @DisplayName("When a new EchoServer object is created, a connection will be made to the provided port number, in this case 5000")
+    void ObjectCreationOpensNewConnection() throws IOException {
+        int port = 5000;
+        ServerSocket serverSocket = new ServerSocket(port);
+        EchoServer echoServer = new EchoServer(serverSocket);
+        ServerSocket connectedServerSocket = echoServer.getServerSocket();
+
+        assertEquals(port, connectedServerSocket.getLocalPort());
+    }
+
+    @Test
+    @DisplayName("When a server socket accepts requests, a client socket will be returned")
+    void createClientSocket() throws IOException {
+        ServerSocket mockServerSocket = mock(ServerSocket.class);
+        EchoServer echoServer = new EchoServer(mockServerSocket);
+
+        when(mockServerSocket.accept()).thenReturn(new Socket());
+
+        assertNotNull(echoServer.createClientSocket(mockServerSocket));
+
+    }
+
+
 }
