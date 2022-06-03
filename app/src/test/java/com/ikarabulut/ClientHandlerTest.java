1fa3ee28c4cf03bb7898154db6c4b8db9b1d3456
Ismail Karabulut <i.karabulut93@gmail.com>
2022-06-03 13:10:03 -0500

    fixes disabled test

--- app/src/test/java/com/ikarabulut/ClientHandlerTest.java
+++ app/src/test/java/com/ikarabulut/ClientHandlerTest.java
@@ -1,15 +1,11 @@
 package com.ikarabulut;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class ClientHandlerTest {
@@ -35,22 +31,23 @@ class ClientHandlerTest {
         verify(io).generateClientSocketReader(clientSocket);
     }
 
-    @Disabled // PROBLEM:: Cannot get the mocks to create actual reader and writer causing Exception to be thrown when .close is called
     @Test
-    @DisplayName("When close() is called, then the clientSocket, writer, and reader should all be closed")
+    @DisplayName("When close() is called, then the clientSocket, writer, and reader should all have .close() called on them")
     void closeClientConnection() throws IOException {
         ServerIO io = mock(ServerIO.class);
         Socket clientSocket = mock(Socket.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        BufferedReader reader = mock(BufferedReader.class);
+
+        when(io.generateClientSocketWriter(clientSocket)).thenReturn(writer);
+        when(io.generateClientSocketReader(clientSocket)).thenReturn(reader);
         ClientHandler clientHandler = new ClientHandler(clientSocket, io);
 
-        when(io.generateClientSocketWriter(clientSocket)).thenReturn(mock(PrintWriter.class));
-
-        PrintWriter writer = clientHandler.getWriter();
-        BufferedReader reader = clientHandler.getReader();
+        clientHandler.closeClientConnection();
 
         verify(writer).close();
         verify(reader).close();
-        assertTrue(clientSocket.isClosed());
-
+        verify(clientSocket).close();
     }
+
 }
