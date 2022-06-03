8231c939a74655631ace141f980924f0ac7f833c
Ismail Karabulut <i.karabulut93@gmail.com>
2022-06-03 11:37:08 -0500

    adds ClientThreadFactory for thread count

--- app/src/main/java/com/ikarabulut/ClientThreadFactory.java
+++ app/src/main/java/com/ikarabulut/ClientThreadFactory.java
@@ -1,2 +1,16 @@
-package com.ikarabulut;public class ClientThreadFactory {
+package com.ikarabulut;
+
+import java.util.concurrent.ThreadFactory;
+
+public class ClientThreadFactory implements ThreadFactory {
+    private int numOfThreads = 0;
+
+    public Thread newThread(Runnable r) {
+        numOfThreads++;
+        return new Thread(r);
+    }
+
+    public int getNumOfThreads() {
+        return numOfThreads;
+    }
 }
--- app/src/test/java/com/ikarabulut/ClientThreadFactoryTest.java
+++ app/src/test/java/com/ikarabulut/ClientThreadFactoryTest.java
@@ -1,4 +1,21 @@
+package com.ikarabulut;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
 class ClientThreadFactoryTest {
-  
+    @Test
+    @DisplayName("When newThread is called the numOfThreads should increase by 1")
+    void newThread_IncreasesNumBy1() {
+        Runnable r = mock(Runnable.class);
+        ClientThreadFactory threadFactory = new ClientThreadFactory();
+
+        assertEquals(0, threadFactory.getNumOfThreads());
+        threadFactory.newThread(r);
+        assertEquals(1, threadFactory.getNumOfThreads());
+    }
+
 }
