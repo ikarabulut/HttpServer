8dd9bebfecb14a9d2966dcd163d418291fc2bf9b
Ismail Karabulut <i.karabulut93@gmail.com>
2022-06-06 15:22:08 -0500

    updates to ThreadPoolExecutor for threading

--- app/src/main/java/com/ikarabulut/App.java
+++ app/src/main/java/com/ikarabulut/App.java
@@ -3,14 +3,17 @@ package com.ikarabulut;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class App {
     public static void main(String[] args) {
         int PORT = 5000;
-        ClientThreadFactory clientThreadFactory = new ClientThreadFactory();
-        ExecutorService threadPool = Executors.newCachedThreadPool(clientThreadFactory);
+        LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(3, 3, 60, SECONDS, linkedBlockingQueue);
 
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
@@ -18,9 +21,14 @@ public class App {
             EchoServer echo = new EchoServer(serverSocket);
             System.out.println("You are running on port: " + PORT);
             while (true) {
-                System.out.println("You have " + clientThreadFactory.getNumOfThreads() + " threads open");
                 Socket clientSocket = echo.createClientSocket();
-                threadPool.submit(new ClientHandler(clientSocket, io));
+                threadPool.execute(new ClientHandler(clientSocket, io));
+                if (threadPool.getActiveCount() == threadPool.getCorePoolSize()) {
+                    System.out.println("You have reached the max core pool size");
+                    System.out.println("You have " + linkedBlockingQueue.size() + " connections in the queue");
+                } else {
+                    System.out.println("You have " + threadPool.getActiveCount() + " active threads");
+                }
             }
         } catch (IOException ex) {
             System.err.print(ex.getMessage());
