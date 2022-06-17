d38957773b88df3c746a4d5c5df000c621e88804
Ismail Karabulut <i.karabulut93@gmail.com>
2022-06-17 11:08:20 -0500

    adds object to handle client inputs

---
+++ app/src/main/java/com/ikarabulut/io/ClientInput.java
@@ -0,0 +1,19 @@
+package com.ikarabulut.io;
+
+import java.io.*;
+
+public class ClientInput {
+    Reader requestReader;
+
+    public ClientInput(InputStream requestInputStream) throws IOException {
+        requestReader = new InputStreamReader(requestInputStream);
+    }
+
+    public String stringifyInput() throws IOException {
+        requestReader = new BufferedReader(requestReader);
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = requestReader.read()) != -1) sb.append((char) c);
+        return sb.toString();
+    }
+}
