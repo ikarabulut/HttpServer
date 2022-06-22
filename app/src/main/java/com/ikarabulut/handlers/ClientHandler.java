0d30cdbafd2dd65bf43d435aa3369652758f6c9e
Ismail Karabulut <i.karabulut93@gmail.com>
2022-06-22 15:04:32 -0500

    updates response based on errors

--- app/src/main/java/com/ikarabulut/handlers/ClientHandler.java
+++ app/src/main/java/com/ikarabulut/handlers/ClientHandler.java
@@ -15,25 +15,29 @@ public class ClientHandler implements Runnable {
     private PrintWriter writer;
     private BufferedReader reader;
     private Socket clientSocket;
+    private InputStream inputStream;
 
     public ClientHandler(Socket clientSocket, ServerIO serverIO) throws IOException {
         this.clientSocket = clientSocket;
         this.serverIO = serverIO;
+        this.inputStream = clientSocket.getInputStream();
         writer = serverIO.generateClientSocketWriter(clientSocket);
         reader = serverIO.generateClientSocketReader(clientSocket);
     }
 
     public void run() {
         try {
-            ClientReader clientReader = new ClientReader(clientSocket.getInputStream());
+            ClientReader clientReader = new ClientReader(inputStream);
             RequestParser requestParser = new RequestParser(clientReader);
             Map<String, String> initialLine = requestParser.parseInitialLine();
 
             Router router = new Router(initialLine);
             Response response = router.routeRequest();
-            System.out.println(response.stringifyResponse());
-
-            serverIO.printOutput(writer, response.stringifyResponse());
+            if (response == null) {
+                serverIO.printOutput(writer, "server error");
+            } else {
+                serverIO.printOutput(writer, response.stringifyResponse());
+            }
             closeClientConnection();
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -42,6 +46,8 @@ public class ClientHandler implements Runnable {
     }
 
     public void closeClientConnection() throws IOException {
+        writer.close();
+        inputStream.close();
         clientSocket.close();
     }
 
