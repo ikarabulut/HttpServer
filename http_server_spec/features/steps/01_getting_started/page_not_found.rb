1cfef306d9424c37eecce48ed16b311f1bd78f3f
Ismail Karabulut <i.karabulut93@gmail.com>
2022-06-23 12:28:22 -0500

    removes spec directory

--- http_server_spec/features/steps/01_getting_started/page_not_found.rb
+++
@@ -1,9 +0,0 @@
-require "./features/steps/shared.rb"
-
-class Spinach::Features::PageNotFound < Spinach::FeatureSteps
-  include Shared::Standard
-
-  step 'I make a GET request to a page that does not exist' do
-    @response = Requests.get("/not_found_resource")
-  end
-end
--- http_server_spec/features/support/env.rb
+++
@@ -1,8 +0,0 @@
-require "rspec/expectations"
-require "yaml"
-
-file_path = File.expand_path(File.join(File.dirname(__FILE__), "config.yml"))
-yaml = YAML.load_file(file_path)
-HOSTNAME = yaml["server"]["hostname"]
-PORT = yaml["server"]["port"]
-PROTOCOL = yaml["server"]["protocol"]
